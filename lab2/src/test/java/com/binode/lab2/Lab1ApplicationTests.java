2ea6827b76f909bbd2c769ebccc4b8c006f30250
Binod Rasaili <binod.toyam@gmail.com>
2024-05-30 10:56:40 -0500

    Done with first part

---
+++ lab2/src/test/java/com/binode/lab2/Lab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binode.lab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
