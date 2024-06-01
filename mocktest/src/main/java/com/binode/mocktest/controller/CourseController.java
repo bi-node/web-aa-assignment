4189e5b20507df37bf6f2c18a14669154f5873bb
Binod Rasaili <binod.toyam@gmail.com>
2024-06-01 16:40:13 -0500

    push before correction

--- mocktest/src/main/java/com/binode/mocktest/controller/CourseController.java
+++ mocktest/src/main/java/com/binode/mocktest/controller/CourseController.java
@@ -1,2 +1,47 @@
-package com.binode.mocktest.controller;public class CourseController {
+package com.binode.mocktest.controller;
+
+import com.binode.mocktest.dto.CourseDTO;
+import com.binode.mocktest.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/courses")
+public class CourseController {
+    private final CourseService courseService;
+
+    @Autowired
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @GetMapping
+    public List<CourseDTO> getAllCourses() {
+        return courseService.getAllCourses();
+    }
+
+    @GetMapping("/{id}")
+    public CourseDTO getCourseById(@PathVariable long id) {
+        return courseService.getCourseById(id);
+    }
+
+    @PostMapping
+    public String addCourse(@RequestBody CourseDTO courseDTO) {
+        courseService.addCourse(courseDTO);
+        return "Course added successfully";
+    }
+
+    @PutMapping
+    public String updateCourse(@RequestBody CourseDTO courseDTO) {
+        courseService.updateCourse(courseDTO);
+        return "Course updated successfully";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteCourseById(@PathVariable long id) {
+        courseService.deleteCourseById(id);
+        return "Course deleted successfully";
+    }
 }
--- mocktest/src/main/java/com/binode/mocktest/controller/StudentController.java
+++ mocktest/src/main/java/com/binode/mocktest/controller/StudentController.java
@@ -1,2 +1,49 @@
-package com.binode.mocktest.controller;public class StudentController {
+package com.binode.mocktest.controller;
+
+import com.binode.mocktest.dto.StudentDTO;
+import com.binode.mocktest.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @PostMapping
+    public String addStudent(@RequestBody StudentDTO studentDTO) {
+        studentService.addStudent(studentDTO);
+        return "Student added successfully";
+    }
+
+    @GetMapping
+    public List<StudentDTO> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    @GetMapping("/{id}")
+    public StudentDTO getStudentById(@PathVariable long id) {
+        return studentService.getStudentById(id);
+    }
+
+    @PutMapping("/{id}")
+    public String updateStudent(@PathVariable long id, @RequestBody StudentDTO studentDTO) {
+        studentDTO.setId(id);
+        studentService.updateStudent(studentDTO);
+        return "Student updated successfully";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteStudent(@PathVariable long id) {
+        studentService.deleteStudentById(id);
+        return "Student deleted successfully";
+    }
 }
